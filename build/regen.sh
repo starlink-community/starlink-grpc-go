5b7e82ec264a966d3dcb9d9bf76c2a7ce1d66bae
Daniel McCarney <daniel@binaryparadox.net>
2022-01-17 19:27:22 -0500

    update generated proto bindings.
    Running `build/get-protoc.sh` gave me an updated `proto/dish.protoset`.
    My router is running `2022.01.0.mr8624-prod` and my dish is running
    `992cafb5-61c7-46a3-9ef7-5907c8cf90fd.uterm.release`.
    
    Afterwards running `build/regen.sh` fails when compiling
    `spacex/api/device/dish.proto` due to a missing reference to
    a `DishConfig` message type. The solution is simple: add another
    `protoc` line to pull `spacex/api/device/dish_config.proto` out of the
    descriptor set as well. Now everything builds nicely :-)

--- build/regen.sh
+++ build/regen.sh
@@ -5,6 +5,7 @@ protoc --go_out=pkg/    --go-grpc_out=pkg/  --descriptor_set_in=proto/dish.proto
 protoc --go_out=pkg/    --go-grpc_out=pkg/  --descriptor_set_in=proto/dish.protoset spacex/api/device/command.proto
 protoc --go_out=pkg/    --go-grpc_out=pkg/  --descriptor_set_in=proto/dish.protoset spacex/api/device/common.proto
 protoc --go_out=pkg/    --go-grpc_out=pkg/  --descriptor_set_in=proto/dish.protoset spacex/api/device/dish.proto
+protoc --go_out=pkg/    --go-grpc_out=pkg/  --descriptor_set_in=proto/dish.protoset spacex/api/device/dish_config.proto
 protoc --go_out=pkg/    --go-grpc_out=pkg/  --descriptor_set_in=proto/dish.protoset spacex/api/device/wifi.proto
 protoc --go_out=pkg/    --go-grpc_out=pkg/  --descriptor_set_in=proto/dish.protoset spacex/api/device/wifi_config.proto
 protoc --go_out=pkg/    --go-grpc_out=pkg/  --descriptor_set_in=proto/dish.protoset spacex/api/device/transceiver.proto
--- go.mod
+++ go.mod
@@ -1,3 +1,12 @@
 module github.com/starlink-community/starlink-grpc-go
 
 go 1.14
+
+require (
+	golang.org/x/net v0.0.0-20220114011407-0dd24b26b47d // indirect
+	golang.org/x/sys v0.0.0-20220114195835-da31bd327af9 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/genproto v0.0.0-20220114231437-d2e6a121cae0 // indirect
+	google.golang.org/grpc v1.43.0
+	google.golang.org/protobuf v1.27.1
+)
